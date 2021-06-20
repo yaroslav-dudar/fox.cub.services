b5b65003c72622675b3e89a64ffc5a7cc19b118b
Yaroslav Dudar <flayingfog@gmail.com>
2021-06-20 15:29:32 +0300

    added multi_season_table function; improved functionality of stats report

--- examples/pg_repo_cli.py
+++ examples/pg_repo_cli.py
@@ -1,6 +1,6 @@
 import asyncio
 import time
-
+import pprint
 from config import Config
 import repository
 
@@ -14,21 +14,29 @@ async def init_task():
     return pg_client
 
 
-async def fetch(match_repo: repository.MatchPgRepository,
-                min_value, max_value, event_ids, season_ids):
+async def fetch_report(match_repo: repository.MatchPgRepository,
+                       min_value, max_value, event_ids, season_ids):
     start_at = time.time()
-    await match_repo.create_season_table()
-    league_table = await match_repo.get_by_league_pos(min_value, max_value, event_ids, season_ids)
-    teams = await match_repo.get_by_score(event_ids, season_ids, 1.0, 3.0, 0.0, 1.1)
-    stats = await match_repo.get_stats([2], [4, 2396, 8668, 8863])
+    await match_repo.create_multi_season_table()
+    low_table_teams = await match_repo.get_by_score(event_ids, season_ids, 0.0, 1.0, 0.0, 3.1)
+    middle_table_teams = await match_repo.get_by_points(event_ids, season_ids, 1.5, 1.8)
+    [print(r) for r in low_table_teams]
+    print("="*20)
+    [print(r) for r in middle_table_teams]
+    print("="*20)
+    stats = await match_repo.get_stats(
+        [2],
+        None,
+        home_teams=[r['team'] for r in middle_table_teams],
+        away_teams=[r['team'] for r in low_table_teams])
+
     print(f"Execution time: {time.time() - start_at}")
-    print(f"Fetched {len(league_table)} records")
-    print(teams)
-    print(dict(stats[0]))
+    print("="*20)
+    pprint.pprint(dict(stats[0]))
 
 
 if __name__ == '__main__':
     loop = asyncio.new_event_loop()
     pg_client = loop.run_until_complete(init_task())
     match_repo = repository.MatchPgRepository(pg_client)
-    loop.run_until_complete(fetch(match_repo, 15, 20, [2], [4, 2396, 8668, 8863]))
+    loop.run_until_complete(fetch_report(match_repo, 15, 20, [2], [4, 2396, 8668, 8863]))
--- repository/pg_repo.py
+++ repository/pg_repo.py
@@ -40,13 +40,17 @@ class MatchPgRepository:
 
 
     async def get_by_points(
-        self, min_points: float, max_points: float,
-        event_ids: list[int], season_ids: list[int],
+        self, event_ids: list[int], season_ids: list[int],
+        min_points: float, max_points: float
     ) -> list[asyncpg.Record]:
+        """
+        Finds teams withing given points per game range.
+        """
         async with self.client.conn_pool.acquire() as con:
             return await con.fetch('''
-                SELECT team, points_per_game from season_table($3, $4)
-                where points_per_game <= $2 and points_per_game >= $1;
+                SELECT team, score_per_game, conceded_per_game, points_per_game
+                FROM multi_season_table($3, $4)
+                WHERE points_per_game <= $2 AND points_per_game >= $1;
             ''', min_points, max_points, event_ids, season_ids)
 
 
@@ -57,8 +61,9 @@ class MatchPgRepository:
     ) -> list[asyncpg.Record]:
         async with self.client.conn_pool.acquire() as con:
             return await con.fetch('''
-                SELECT team, score_per_game, conceded_per_game from season_table($1, $2)
-                where (score_per_game <= $4 AND score_per_game >= $3)
+                SELECT team, score_per_game, conceded_per_game, points_per_game
+                FROM multi_season_table($1, $2)
+                WHERE (score_per_game <= $4 AND score_per_game >= $3)
                     AND (conceded_per_game <= $6 AND conceded_per_game >= $5);
             ''', event_ids, season_ids, min_score, max_score, min_conceded, max_conceded)
 
@@ -81,22 +86,54 @@ class MatchPgRepository:
 
 
     async def get_stats(self,
-                        event_ids: list[int],
-                        season_ids: list[int],
-                        game_ids: Optional[list[int]] = None):
+                        event_ids: Optional[list[int]] = None,
+                        season_ids: Optional[list[int]] = None,
+                        home_teams: Optional[list[str]] = None,
+                        away_teams: Optional[list[str]] = None):
          async with self.client.conn_pool.acquire() as con:
-            return await con.fetch('''
-                SELECT avg(away_team_points) AS away_points,
+            base_query = '''SELECT avg(away_team_points) AS away_points,
                        avg(home_team_points) AS home_points,
                        count(*) AS total_games,
                        avg(away_team_score) AS away_goals,
                        avg(home_team_score) AS home_goals,
                        avg(home_team_score) + avg(away_team_score) AS goals_per_game,
+
+                       count(id) FILTER (WHERE away_team_score + home_team_score > 2.5) / cast(count(*) AS decimal) AS over2_5,
+                       count(id) FILTER (WHERE away_team_score + home_team_score > 3.5) / cast(count(*) AS decimal) AS over3_5,
+                       count(id) FILTER (WHERE away_team_score > 0 AND home_team_score > 0) / cast(count(*) AS decimal) AS btts,
+
                        count(id) FILTER (WHERE away_team_score > home_team_score) / cast(count(*) AS decimal) AS away_win,
                        count(id) FILTER (WHERE away_team_score < home_team_score) / cast(count(*) AS decimal) AS home_win,
                        count(id) FILTER (WHERE away_team_score = home_team_score) / cast(count(*) AS decimal) AS draw
-                from game where event_id = any($1::int[]) AND season_id = any($2::int[]);
-            ''', event_ids, season_ids)
+                from game where {0} AND {1} AND {2} AND {3};'''
+
+            args = []
+            if event_ids:
+                args.append(event_ids)
+                event_where = f'event_id = ANY(${len(args)}::int[])'
+            else:
+                event_where = 'event_id IS NOT NULL'
+
+            if season_ids:
+                args.append(season_ids)
+                season_where = f'season_id = ANY(${len(args)}::int[])'
+            else:
+                season_where = 'season_id IS NOT NULL'
+
+            if home_teams:
+                args.append(home_teams)
+                home_teams_where = f'home_team = ANY(${len(args)}::varchar[])'
+            else:
+                home_teams_where = 'home_team IS NOT NULL'
+
+            if away_teams:
+                args.append(away_teams)
+                away_teams_where = f'away_team = ANY(${len(args)}::varchar[])'
+            else:
+                away_teams_where = 'away_team IS NOT NULL'
+
+            final_query = base_query.format(event_where, season_where, home_teams_where, away_teams_where)
+            return await con.fetch(final_query, *args)
 
 
     async def create_season_table(self) -> None:
@@ -134,6 +171,42 @@ class MatchPgRepository:
                     ) as query
                     GROUP BY team, season_id
 
+                $$ LANGUAGE SQL STABLE;''',)
+
+
+    async def create_multi_season_table(self) -> None:
+        async with self.client.conn_pool.acquire() as con:
+            return await con.execute('''
+                CREATE OR REPLACE FUNCTION multi_season_table(events integer[], seasons integer[])
+                RETURNS TABLE(team VARCHAR,
+                              points int,
+                              points_per_game float,
+                              score_per_game float,
+                              conceded_per_game float)
+                AS $$
+                    SELECT
+                        team, sum(points) AS points,
+                        sum(points) / sum(games) AS points_per_game,
+                        sum(score) / sum(games) AS score_per_game,
+                        sum(conceded) / sum(games) AS conceded_per_game FROM
+
+                    (SELECT away_team AS team, sum(away_team_points) AS points,
+                        sum(away_team_score) AS score, sum(home_team_score) AS conceded,
+                        count(*) AS games
+                        FROM game WHERE event_id = ANY(events)
+                        AND season_id = ANY(seasons)
+                        GROUP BY away_team
+
+                    UNION
+
+                    SELECT home_team AS team, sum(home_team_points) AS points,
+                        sum(home_team_score) AS score, sum(away_team_score) AS conceded,
+                        count(*) AS games
+                        FROM game WHERE event_id = ANY(events)
+                        AND season_id = ANY(seasons)
+                        GROUP BY home_team
+                    ) as query
+                    GROUP BY team
                 $$ LANGUAGE SQL STABLE;''',)
 
 
